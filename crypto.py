e0620b111bb79bee60a118ba3b9cff975c3d2d16
Guillaume Aubertin <guillaume.aubertin@gmail.com>
2017-09-01 16:15:26 +0200

    wip: rework
    Signed-off-by: Guillaume Aubertin <guillaume.aubertin@gmail.com>

--- crypto.py
+++ crypto.py
@@ -1,6 +1,5 @@
 import time, json, bittrex
 
-
 class bcolors:
         HEADER = '\033[95m'
         OKBLUE = '\033[94m'
@@ -19,29 +18,29 @@ class coin:
         balance: current balance in wallet.
         current_price: current price on the market
         orders: order history
-        open_orders:
+        open_orders: on-going orders
+        summaries: market summaries
     """
 
         def __init__(self, name, balance):
                 self.name = name
                 self.balance = float(balance)
                 self.orders = []
-                self.last_price = 0.0
                 self.open_orders = []
+                self.coin_summaries = []
 
         def print_gain(self):
                 for order in self.orders:
                         # get latest LIMIT_BUY
                         if order["OrderType"] == "LIMIT_BUY":
                                 buy_price = float(order["PricePerUnit"])
-                                gain = (self.last_price - buy_price) / buy_price * 100
+                                gain = (float(self.coin_summaries[0]["Last"]) - buy_price) / buy_price * 100
 
                                 if float(gain) > 0:
                                         print bcolors.OKGREEN + self.name + " : " + str(gain) + bcolors.ENDC
                                 else :
                                         print bcolors.FAIL + self.name + " : " + str(gain) + bcolors.ENDC
                                 return gain
-
 
 class bittrex_wallet:
         """bittrex wallet
@@ -73,19 +72,21 @@ class bittrex_wallet:
                         self.coinlist.append(coin(name, balance))
                         print "new coin", name, "created with balance", balance
 
-                # fill coinlist with order history, open orders and last price
-                # latest order comes first in the list
+                # fill coinlist with order history, open orders and market summary
+                # for orders and history, latest comes first in the list
+                # for market summary, only one to append. next iterations will 'insert' in first position
                 for newcoin in self.coinlist:
 
-                        if newcoin.name == "BTC":
-                                newcoin.last_price = 1
-                                continue
+#                        if newcoin.name == "BTC":
+#                                newcoin.last_price = 1
+#                                continue
 
                         market_name = "BTC-" + newcoin.name
 
                         for entry in self.market["result"]:
                                 if market_name in entry["MarketName"]:
-                                        newcoin.last_price = float(entry["Last"])
+                                        newcoin.coin_summaries.append(entry)
+                                        print json.dumps(entry, indent=4, sort_keys=True)
                                         break
 
                         for order in self.orderhistory["result"]:
@@ -95,6 +96,64 @@ class bittrex_wallet:
                         for order in self.openorders["result"]:
                                 if market_name in order["Exchange"]:
                                         newcoin.open_orders.append(order)
+                                        
+        # update coinlist
+        def update(self):
+
+                self.fetch_data()
+
+                # check for new coins
+                for x in self.balance["result"]:
+                        name = x["Currency"]
+                        balance = x["Balance"]
+                        # test if no balance or already created coin object
+                        if balance == 0.0 or name in self.blacklist:
+                                continue
+                        for x in self.coinlist:
+                                if x.name == name:
+                                        x.balance = float(balance)
+                                        break
+                        else:
+                                self.coinlist.append(coin(name, balance))
+                                print "new coin", name, "has been added to the wallet"
+                                
+
+                # check for removed coins
+                for entry in self.coinlist[:]:
+                        for x in self.balance["result"]:
+                                name = x["Currency"]
+                                balance = x["Balance"]
+                                if name == entry.name and balance == 0.0:
+                                        print "removing", name, "from coinlist"
+                                        coinlist.remove(entry)
+                
+                # update summaries
+                for x in self.coinlist:
+                        market_name = "BTC-" + x.name
+                        for entry in self.market["result"]:
+                                if market_name in entry["MarketName"]:
+                                        x.coin_summaries.insert(0, entry)
+                                        break
+
+
+                # update order history and open orders
+                # TODO
+                for x in self.coinlist:
+                        market_name = "BTC-" + x.name
+                        for entry in self.orderhistory["result"]:
+                                if entry not in x.orders:
+                                        x.orders.insert(0, entry)
+                                        print "adding new orderhistory for", market_name
+                        for entry in self.openorders["result"]:
+                                if entry not in x.open_orders:
+                                        x.open_orders.insert(0, entry)
+                                        print "adding new openorder for", market_name
+                        
+
+                
+                # sort list based on coin names
+                self.coinlist.sort(key=lambda x: x.name)
+
 
         # get data from exchange
         def fetch_data(self):
@@ -116,50 +175,14 @@ class bittrex_wallet:
                 print("fetch_exchange execution time : %g seconds" % (end_time - start_time))
 
 
-        # update coinlist
-        def update(self):
 
-                self.fetch_data()
-
-                # check for new coins
-                for x in self.balance["result"]:
-                        name = x["Currency"]
-                        balance = x["Balance"]
-                        # test if no balance or already created coin object
-                        if balance == 0.0 or name in self.blacklist:
-                                continue
-                        for x in self.coinlist:
-                                if x.name == name:
-                                        x.balance = float(balance)
-                                        break
-                        else:
-                                self.coinlist.append(coin(name, balance))
-                                print "new coin", name, "has been added to the wallet"
-
-                # update current price
-                for x in self.coinlist:
-
-                        if x.name == "BTC":
-                                x.last_price = 1
-                                continue
-
-                        market_name = "BTC-" + x.name
-
-                        for entry in self.market["result"]:
-                                if market_name in entry["MarketName"]:
-                                        x.last_price = float(entry["Last"])
-                                        break
-
-
-                        # sort list based on coin names
-                        self.coinlist.sort(key=lambda x: x.name)
-
+                
 
         # calculate current wallet value in BTC
         def value(self):
                 total = 0.0
                 for coin in self.coinlist:
-                        total = total + (coin.last_price * coin.balance)
+                        total = total + (float(coin.coin_summaries[0]["Last"]) * coin.balance)
                 print "total value is " + str(total) + " BTC"
 
         # print gains for each coin with positive balance
